98abdf3c434c6c7a2aea269c942096c8e62eeebe
nancy <nancy@192.168.20.66>
2022-05-12 20:06:05 +0800

    version1

---
+++ index.js
@@ -0,0 +1,42 @@
+const canvas = document.querySelector("#draw");
+
+const ctx = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.strokeStyle = "#BADA55";
+ctx.lineJoin = "round"; // end of the line
+ctx.lineCap = "round"; // a line meets another line
+ctx.lineWidth = 30;
+
+let isDrawing = false;
+let lastX = 0;
+let lastY = 0;
+let hue = 0;
+
+function draw(e) {
+  console.log(e);
+  if (!isDrawing) return;
+  ctx.strokeStyle = `hsl(${hue},100%, 50%, .9)`
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);          // start from
+  ctx.lineTo(e.offsetX, e.offsetY);  // go to 
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+  lastX = e.offsetX;
+  lastY = e.offsetY;
+  hue += 3;
+//   if(hue >= 360) {
+//     hue = 0;
+//   }
+
+}
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", (e) => {
+    isDrawing = true;
+    [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener("mouseup", () => (isDrawing = false));
+canvas.addEventListener("mouseout", () => (isDrawing = false));
